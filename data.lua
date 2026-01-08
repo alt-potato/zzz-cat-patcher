dac2b8a7eb95f694ecb3958b18356d0a153000fe
alt-potato <jason.why.zhuang@gmail.com>
2026-01-07 21:08:35 -0500

    Add protogen character patch

---
+++ data.lua
@@ -0,0 +1,9 @@
+local protogen_corpse = data.raw["character-corpse"]["protogen-skin-corpse"]
+
+protogen_corpse.armor_picture_mapping = {
+    ["heavy-armor"] = 2,
+    ["light-armor"] = 2,
+    ["modular-armor"] = 3,
+    ["power-armor"] = 3,
+    ["power-armor-mk2"] = 3
+}
