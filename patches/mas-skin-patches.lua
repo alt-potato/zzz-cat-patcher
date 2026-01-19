a22a9bd53f205c4b397639a8eff8fe8a1068d9fa
alt-potato <jason.why.zhuang@gmail.com>
2026-01-19 02:08:19 -0500

    Add skin patch for wickerbeast skins

--- patches/mas-skin-patches.lua
+++ patches/mas-skin-patches.lua
@@ -2,24 +2,54 @@ if not mods["metal-and-stars"] then
 	return
 end
 
+---@class CorpsePatch
+---@field corpse_name string
+---@field prototype table -- from charactermodhelper
+---@field armor_picture_mapping nil|table<string, number> -- note that "" is used as a fallback
+
+---@type table<string, CorpsePatch>
 local to_patch = {
-	["ProtogenCharacterUpdated"] = { "protogen-skin-corpse", PROTOCHAR.protos, nil },
-	["koboldengineer"] = { "koboldengineer-skin-corpse", KOBOLD, nil },
+	["ProtogenCharacterUpdated"] = { corpse_name = "protogen-skin-corpse", prototype = PROTOCHAR.protos },
+	["koboldengineer"] = { corpse_name = "koboldengineer-skin-corpse", prototype = KOBOLD },
+	["wickerbeast"] = {
+		corpse_name = "_wickerbeast-corpse",
+		prototype = _3aecca88_5f97_447e_b7dd_46f834ad3c03,
+		armor_picture_mapping = {
+			["light-armor"] = 1,
+			[""] = 2,
+		},
+	},
+	["lexxy-wickerbeast"] = {
+		corpse_name = "_f42f38fb_b84e_4b34_8fd8_70cce8873d5b",
+		prototype = _3aecca88_5f97_447e_b7dd_46f834ad3c03,
+		armor_picture_mapping = {
+			["light-armor"] = 1,
+			[""] = 2,
+		},
+	},
 }
 
 for mod_name, patch_data in pairs(to_patch) do
-	if mods[mod_name] then
-		log("patching " .. mod_name .. " with " .. patch_data[1])
+	local corpse = data.raw["character-corpse"][patch_data.corpse_name]
 
-		local corpse = data.raw["character-corpse"][patch_data[1]]
+	if mods[mod_name] and corpse then
+		log("patching " .. mod_name .. " with corpse " .. patch_data.corpse_name .. "...")
 
-		-- just set them all to 1 (scuffed)
+		-- just set them all to 1 (scuffed) (unless a predefined mapping is given)
 		corpse.armor_picture_mapping = {}
 		for a_name, armor in pairs(data.raw.armor) do
-			corpse.armor_picture_mapping[a_name] = to_patch[mod_name][3] or 1
+			if patch_data.armor_picture_mapping then
+				corpse.armor_picture_mapping[a_name] = patch_data.armor_picture_mapping[a_name]
+					or patch_data.armor_picture_mapping[""]
+					or 1
+			else
+				corpse.armor_picture_mapping[a_name] = 1
+			end
 		end
 
 		-- let charactermodhelper know we changed stuff
-		patch_data[2].corpse.armor_picture_mapping = table.deepcopy(corpse.armor_picture_mapping)
+		patch_data.prototype.corpse.armor_picture_mapping = table.deepcopy(corpse.armor_picture_mapping)
+	elseif not corpse then
+		log("no corpse " .. patch_data.corpse_name .. " for " .. mod_name .. ".")
 	end
 end
