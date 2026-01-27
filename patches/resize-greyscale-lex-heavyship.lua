07688eaf844b9200a076165ba481e3030418dd58
alt-potato <jason.why.zhuang@gmail.com>
2026-01-27 18:24:28 -0500

    Modify greyscale patch to use existing assets

--- patches/resize-greyscale-lex-heavyship.lua
+++ patches/resize-greyscale-lex-heavyship.lua
@@ -2,45 +2,25 @@ if not (mods["lex-aircraft"] and mods["lex-aircraft-grayscale"]) then
 	return
 end
 
----@param name string
----@param layer integer
----@param opts table<integer, table<string, any>>
-local replace_stripes = function(name, layer, opts)
-	local ship = data.raw["spider-vehicle"][name]
-
-	if
-		-- overkill? no such thing!
-		ship
-		and ship.graphics_set
-		and ship.graphics_set.animation
-		and ship.graphics_set.animation.layers
-		and ship.graphics_set.animation.layers[layer]
-		and ship.graphics_set.animation.layers[layer].stripes
-	then
-		local stripes = ship.graphics_set.animation.layers[layer].stripes
-
-		for i, mods in pairs(opts) do
-			local stripe = stripes[i]
-
-			if stripe then
-				for k, mod in pairs(mods) do
-					stripe[k] = mod
-				end
-			end
-		end
-	else
-		log("failed to patch " .. name .. " (layer " .. layer .. ")")
-	end
-end
-
 local heavyship_name = "lex-flying-heavyship"
-local heavyship_layer = 1
-local heavyship_opts = {
-	[1] = { filename = "__zzz-cat-patcher__/graphics/entity/flying_heavyship/hr-flying_heavyship_1-64.png" },
-	[2] = { filename = "__zzz-cat-patcher__/graphics/entity/flying_heavyship/hr-flying_heavyship_65-128.png" },
-}
+local heavyship = data.raw["spider-vehicle"][heavyship_name]
 
 -- should be 5600x5600 instead of 4000x4000
 log("patching grayscale " .. heavyship_name .. " (4000x4000 -> 5600x5600)...")
 --             /e\
-replace_stripes(heavyship_name, heavyship_layer, heavyship_opts)
+-- replace_stripes(heavyship_name, heavyship_layer, heavyship_opts)
+
+if
+    heavyship
+    and heavyship.graphics_set
+    and heavyship.graphics_set.animation
+    and heavyship.graphics_set.animation.layers
+    and heavyship.graphics_set.animation.layers[1]
+then
+    local layer = heavyship.graphics_set.animation.layers[1]
+
+    -- WAIT I'M A GENIUS
+    layer.width = layer.width * (4000 / 5600)
+    layer.height = layer.height * (4000 / 5600)
+    layer.scale = layer.scale * (5600 / 4000)
+end
