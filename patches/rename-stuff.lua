b548035c9e9cbdeca233e8ece863daf6e01ccac2
alt-potato <jason.why.zhuang@gmail.com>
2026-01-28 15:15:06 -0500

    Add structure and tech from ms-quantum-stabilizer

--- patches/rename-stuff.lua
+++ patches/rename-stuff.lua
@@ -10,17 +10,34 @@ end
 
 if mods["rubia"] then
 	-- conflicts with efficiency module 4 from secretas
-	modify_prototype({ "module", "rubia-efficiency-module4" }, { localised_name = { "cat-patch-item-name.rubia-efficiency-module4" } })
+	modify_prototype(
+		{ "module", "rubia-efficiency-module4" },
+		{ localised_name = { "cat-patch-item-name.rubia-efficiency-module4" } }
+	)
 end
 
 if mods["metal-and-stars"] then
 	-- capitalize first letter
 	modify_prototype({ "ammo", "gauss-rocket" }, { localised_name = { "cat-patch-item-name.gauss-rocket" } })
-    -- conflicts with quantum stabilizer from age of production
-    modify_prototype({ "item", "ms-quantum-stabilizer" }, { localised_name = { "cat-patch-item-name.ms-quantum-stabilizer" } })
+	-- conflicts with quantum stabilizer from age of production
+	modify_prototype(
+		{ "item", "ms-quantum-stabilizer" },
+		{ localised_name = { "cat-patch-item-name.ms-quantum-stabilizer" } }
+	)
+	modify_prototype(
+		{ "assembling-machine", "ms-quantum-stabilizer" },
+		{ localised_name = { "cat-patch-item-name.ms-quantum-stabilizer" } }
+	)
+	modify_prototype({ "technology", "quantum-stabilization" }, {
+		localised_name = { "cat-patch-technology-name.quantum-stabilization" },
+		localised_description = { "cat-patch-technology-name.quantum-stabilization" },
+	})
 end
 
 if mods["NapalmArtillery"] then
 	-- uncapitalize subsequent words
-	modify_prototype({ "ammo", "napalm-artillery-shell" }, { localised_name = { "cat-patch-item-name.napalm-artillery-shell" } })
+	modify_prototype(
+		{ "ammo", "napalm-artillery-shell" },
+		{ localised_name = { "cat-patch-item-name.napalm-artillery-shell" } }
+	)
 end
