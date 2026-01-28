9cd0c5327b591961a79f1cebaca3a60c7ec1fea8
alt-potato <jason.why.zhuang@gmail.com>
2026-01-28 14:37:17 -0500

    Correct item categories

--- patches/rename-stuff.lua
+++ patches/rename-stuff.lua
@@ -3,22 +3,22 @@ local modify_prototype = require("lib.manipulation").modify_prototype
 if mods["Cerys-Moon-of-Fulgora"] then
 	-- conflicts with overclock modules from metal-and-stars
 	modify_prototype(
-		{ "item", "cerys-overclock-module" },
+		{ "module", "cerys-overclock-module" },
 		{ localised_name = { "cat-patch-item-name.cerys-overclock-module" } }
 	)
 end
 
 if mods["rubia"] then
 	-- conflicts with efficiency module 4 from secretas
-	modify_prototype({ "item", "rubia-efficiency-module4" }, { localised_name = { "cat-patch-item-name.rubia" } })
+	modify_prototype({ "module", "rubia-efficiency-module4" }, { localised_name = { "cat-patch-item-name.rubia-efficiency-module4" } })
 end
 
 if mods["metal-and-stars"] then
 	-- capitalize first letter
-	modify_prototype({ "item", "gauss-rocket" }, { localised_name = { "cat-patch-item-name.gauss-rocket" } })
+	modify_prototype({ "ammo", "gauss-rocket" }, { localised_name = { "cat-patch-item-name.gauss-rocket" } })
 end
 
 if mods["NapalmArtillery"] then
 	-- uncapitalize subsequent words
-	modify_prototype({ "item", "napalm-artillery-shell" }, { localised_name = { "cat-patch-item-name.napalm-rocket" } })
+	modify_prototype({ "ammo", "napalm-artillery-shell" }, { localised_name = { "cat-patch-item-name.napalm-artillery-shell" } })
 end
